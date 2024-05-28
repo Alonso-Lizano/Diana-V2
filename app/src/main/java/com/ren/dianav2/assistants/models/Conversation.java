4dcfd209c3a997d98604890560c99874c1656a74
Luis Miguel Mendez <luismii.mme9@gmail.com>
2024-05-28 13:07:00 +0200

    Chat database improved

--- app/src/main/java/com/ren/dianav2/assistants/models/Conversation.java
+++ app/src/main/java/com/ren/dianav2/assistants/models/Conversation.java
@@ -8,14 +8,18 @@ public class Conversation {
     private String id;
     private String userId;
     private List<MessageRequest> messages;
+    private String idAssistant;
+    private String title;
 
     public Conversation() {
     }
 
-    public Conversation(String id, String userId, List<MessageRequest> messages) {
+    public Conversation(String id, String userId, List<MessageRequest> messages, String idAssistant, String title) {
         this.id = id;
         this.userId = userId;
         this.messages = messages;
+        this.idAssistant = idAssistant;
+        this.title = title;
     }
 
     public String getId() {
@@ -40,5 +44,21 @@ public class Conversation {
 
     public void setMessages(List<MessageRequest> messages) {
         this.messages = messages;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIdAssistant() {
+        return idAssistant;
+    }
+
+    public void setIdAssistant(String idAssistant) {
+        this.idAssistant = idAssistant;
     }
 }
